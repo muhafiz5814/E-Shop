9658123138db7a4bc38895b9cfcb30c46c0e08a6
Muhafiz Raza <muhafizraza1209@gmail.com>
2024-01-14 10:37:12 +0530

    Make Search feature

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -3,7 +3,15 @@ const Product = require("../models/product.js")
 const router = express.Router()
 
 router.get("/products", async (req, res) => {
-    const allProducts = await Product.find({})
+
+    const search = req.query.search || ""
+
+    const allProducts = await Product.find({
+        $or:[
+            { title: { $regex: ".*"+search+".*", $options:"i" } },
+            { description: { $regex: ".*"+search+".*", $options:"i" } }
+        ]
+    })
     res.render("products", {products: allProducts})
 })
 
